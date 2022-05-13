e36f184db1c0e82495343704c46a12ff4d0e0edf
Abdulgafaar <Abdulgafaar>
2022-05-13 22:33:08 +0100

    modified the backend config file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,19 @@
-provider "aws" {
-  region = var.region
+terraform {
+  backend "s3" {
+    bucket = "abdul-terraform-backend-state"
+    key = "terraform.tfstate"
+    region = "eu-west-1"
+    dynamodb_table = "terraform-statetable"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
-terraform {
-  backend "s3" {}
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
 }
